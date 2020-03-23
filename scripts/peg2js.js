c8a4ccc37cf85d74fdf4332e49089d5e774ceda6
misogihagi <47350059+misogihagi@users.noreply.github.com>
2020-03-23 15:48:48 +1000

    'テストの追加とそれに合わせた変更'

--- scripts/peg2js.js
+++ scripts/peg2js.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 console.log(__dirname)
 const text = fs.readFileSync(__dirname + "/../peg.txt", 'utf-8');
 const parser = peg.generate(text, {
-  output: "source"
+  output: "source",
+  cache: true
 })
 fs.writeFile(__dirname + '/../nakoparser.js', 'const a=' + parser + '\nmodule.exports=a', () => {})
